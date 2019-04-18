9d281a8af10e3dce0d617d72ccb963777cae4c58
tryggves <tryggve.sorensen@gmail.com>
2019-04-18 06:51:12 +0200

    Template iteration 1.

--- template_ex01.cpp
+++ template_ex01.cpp
@@ -3,6 +3,13 @@
 //
 # include <iostream>
 
+// Try to code a specific class first an then refactor to template class
+class Calc {
+public:
+    int add (int a, int b) { return a + b; }
+    int sub (int a, int b) { return a - b; }
+};
+
 int main(int argc, char* argv[]) {
     std::cout << "==========================================================================" << std::endl;
     std::cout << "=== REFERENCES LVALUE RVALUE EXAMPLES                                 ====" << std::endl;
@@ -10,4 +17,8 @@ int main(int argc, char* argv[]) {
     std::cout << "=== Number of arguments: " << argc << std::endl;
     std::cout << "==========================================================================" << std::endl;
 
+    Calc myCalc;
+    int c = myCalc.add(1, 2);
+    std::cout << "1 + 2 = " << c << std::endl;
+    std::cout << "1 - 2 = " << myCalc.sub(1, 2) << std::endl;
 }
