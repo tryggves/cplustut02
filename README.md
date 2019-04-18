5f58a9b368a518a0b4ae63af497ded75eba7e015
tryggves <tryggve.sorensen@gmail.com>
2019-04-18 06:38:22 +0200

    Adding new example for templates.

---
+++ README.md
@@ -0,0 +1,17 @@
+Self training on C++ coding.
+
+
+ * Some ideas:
+ * pointers and references
+ * auto
+ * templates
+ * type casting
+ * standard library - selected topics
+ 
+Functional: Standard library feature for working with functions.
+
+References: Handling of references, pointers, dynamic memory
+allocation etc.
+
+Templates: Generic programming - coding with templates and applying
+Standard Template Library.
---
+++ template_ex01.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Tryggve Sørensen on 2019-04-18.
+//
+# include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "==========================================================================" << std::endl;
+    std::cout << "=== REFERENCES LVALUE RVALUE EXAMPLES                                 ====" << std::endl;
+    std::cout << "=== Program name: " << argv[0] << std::endl;
+    std::cout << "=== Number of arguments: " << argc << std::endl;
+    std::cout << "==========================================================================" << std::endl;
+
+}
