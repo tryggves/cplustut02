9ba8584cfd7a6874a52781a5fd622cf498f14b84
tryggves <tryggve.sorensen@gmail.com>
2019-04-20 09:41:46 +0200

    Adding custom class to vector and overloaded outputstream.

--- templates/template_ex02.cpp
+++ templates/template_ex02.cpp
@@ -8,6 +8,20 @@
 #include <iostream>
 #include <vector>
 
+// EXAMPLE 2: Define class to be contained in vector
+template <class T>
+class MyNumContainer {
+    T m_value;
+
+public:
+    MyNumContainer (T val) : m_value (val) {};
+
+    // Here is how to overload the output stream operator
+    friend std::ostream& operator<<( std::ostream& output, const MyNumContainer& mNC ) {
+        output << mNC.m_value;
+        return output;
+    }
+};
 
 int main(int argc, char **argv) {
     std::cout << "==========================================================================" << std::endl;
@@ -30,6 +44,20 @@ int main(int argc, char **argv) {
 
     // Notice the compact syntax for accessing values of vector in the loop.
     for (int x:intVec) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+
+    // Try out the custom class example
+    std::cout << "EXAMPLE 2: CUSTOM CLASS IN VECTOR" << std::endl;
+    std::vector<MyNumContainer<int>> mNCvec;
+    for (i=0; i<10; i++) {
+        MyNumContainer<int> mnc(i);
+        mNCvec.push_back(mnc);
+    }
+    std::cout << "Size of mNCvec: " << mNCvec.size() << std::endl;
+    std::cout << "mNCvec contains: ";
+    for (MyNumContainer<int> x:mNCvec) {
         std::cout << x << " ";
     }
     std::cout << std::endl;
