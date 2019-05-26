2824f75e84b8b35c1ec35c00124f41b5cdf8992d
tryggve <tryggve.sorensen@me.com>
2019-05-26 08:54:34 +0200

    Adding thread examples.

---
+++ threads/thread_ex01.cpp
@@ -0,0 +1,13 @@
+//
+// Created by tryggve on 5/26/19.
+//
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "==========================================================================" << std::endl;
+    std::cout << "=== CLASSES EXAMPLE 02                                                ====" << std::endl;
+    std::cout << "=== Program name: " << argv[0] << std::endl;
+std::cout << "=== Number of arguments: " << argc << std::endl;
+
+}
