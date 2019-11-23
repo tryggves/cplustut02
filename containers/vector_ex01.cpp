e9e4e78a37f977e172f5a80e0565b52a667d87af
tryggves <tryggve.sorensen@gmail.com>
2019-11-23 16:47:51 +0100

    Container examples

--- containers/vector_ex01.cpp
+++ containers/vector_ex01.cpp
@@ -4,7 +4,70 @@
 
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <map>
+
+/**
+ * Deque is a double ended array where it is cheap to insert elements
+ * in the front and the end.
+ */
+void example3 () {
+    std::cout << "==========================================================================\n";
+    std::cout << "EXAMPLE 3: DEQUE\n";
+    std::cout << "==========================================================================\n";
+
+    std::deque<float> myFltDeque;       // Declares empty
+
+    // Notice this initialization of i in the for loop
+    for (int i : {1,2,3,4,5,6}) {
+        std::cout << "i: " << i << "\n";
+        // This pushes numbers to the front of the vector
+        myFltDeque.push_front(i*1.1);
+    }
+
+    // Iterate through the array
+    std::cout << "myFltDeque: ";
+    for (auto e : myFltDeque) {
+        std::cout << e << ", ";
+    }
+    std::cout << "\n";
+
+    // Push to back of vector
+    // Notice this initialization of i in the for loop
+    std::cout << "Push to back of vector...\n";
+    for (int i : {1,2,3,4,5,6}) {
+        std::cout << "i: " << i << "\n";
+        // This pushes numbers to the back of the vector
+        myFltDeque.push_back(i*1.1);
+    }
+
+    // Iterate through the array
+    std::cout << "myFltDeque: ";
+    for (auto e : myFltDeque) {
+        std::cout << e << ", ";
+    }
+    std::cout << "\n";
+}
+
+
+void example2 () {
+    std::cout << "==========================================================================\n";
+    std::cout << "EXAMPLE 2: VECTOR\n";
+    std::cout << "==========================================================================\n";
+    std::vector<int> myIntVec {1, 2, 3, 4, 5, 6};
+    int sum {0};
+
+    std::cout << "Size: " << myIntVec.size() << "\n";
+    for (auto x : myIntVec) {
+        std::cout << "Adding " << x << " to sum\n";
+        sum += x;
+    }
+    std::cout << "Sum: " << sum << "\n";
+
+    // access elements of vector
+    sum = myIntVec[3] + myIntVec[4];
+    std::cout << "Sum: " << sum << "\n";
+}
 
 int main (int argc, char **argv){
     std::cout << "==========================================================================" << std::endl;
@@ -29,5 +92,8 @@ int main (int argc, char **argv){
     std::vector<std::map<std::string, std::string>> listeners;
     listeners.push_back(pMap1);
     listeners.push_back(pMap2);
+
+    example2();
+    example3();
 
 }
