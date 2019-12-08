6dbab497cc977a8b661c3c4bbc858ed6473d4d3d
tryggves <tryggve.sorensen@gmail.com>
2019-12-08 23:23:56 +0100

    Comment patch.

--- classes/MyOtherClass.h
+++ classes/MyOtherClass.h
@@ -8,6 +8,12 @@
 #include <string>
 #include <memory>
 #include "MyHandler.h"
+/**
+ * Example of two classes cross referencing each other. This is
+ * implemented using forward declaration of the other class in this
+ * header file as next statement. But to make this work there cannot be
+ * any implementation dependencies in the header.
+ */
 class MyHandler;
 
 class MyOtherClass {
