5e88a49b470016ff03dcf7a7d6ea14fed4a6a365
trysoe <tryggve.sorensen@pgs.com>
2022-11-24 08:31:11 +0100

    Time and date example. Small refactor of MyLocalTime class.

--- time_ex.cpp
+++ time_ex.cpp
@@ -13,24 +13,28 @@
 
 class MyLocalTime {
 private:
-  std::chrono::system_clock::time_point m_timePoint;
+  std::time_t m_timestampEpoch;
 public:
   // Default constructor initialized time to now
   MyLocalTime() {
-    m_timePoint = std::chrono::system_clock::now();
+    std::chrono::system_clock::time_point timePoint = std::chrono::system_clock::now();
+    m_timestampEpoch = std::chrono::system_clock::to_time_t(timePoint);
+  }
+
+  // Constructor with timestamp in seconds since epoch
+  MyLocalTime(long secsSinceEpoch) {
+    m_timestampEpoch = secsSinceEpoch;
   }
 
   std::string calendarTimeAsStr() const {
-    const std::time_t myTime = std::chrono::system_clock::to_time_t(m_timePoint);
-    std::string myTimeStr = std::ctime(&myTime);    // Convert to calendar time
+    std::string myTimeStr = std::ctime(&m_timestampEpoch);    // Convert to calendar time
     myTimeStr.resize(myTimeStr.size()-1);       // Skip trailing newline
 
     return myTimeStr;
   }
 
   long epochTime() const {
-    const std::time_t myTime = std::chrono::system_clock::to_time_t(m_timePoint);
-    return myTime;
+    return m_timestampEpoch;
   }
 };
 
@@ -44,16 +48,14 @@ int main(int argc, char **argv) {
   }
   std::cout << "==========================================================================" << std::endl;
 
-  // Replace with class implementation
-  //  std::chrono::system_clock::time_point timePoint;
-  //  timePoint = std::chrono::system_clock::now();
-  //  const std::time_t myTime = std::chrono::system_clock::to_time_t(timePoint);
-  //  std::string myTimeStr = std::ctime(&myTime);    // Convert to calendar time
-  //  myTimeStr.resize(myTimeStr.size()-1);       // Skip trailing newline
-  //  std::cout << "Calendar time: " << myTimeStr << std::endl;
-
+  std::cout << "Initialize time to now..." << std::endl;
   MyLocalTime localTime;
   std::cout << "Calendar time: " << localTime.calendarTimeAsStr() << std::endl;
   std::cout << "Epoch time: " << localTime.epochTime() << std::endl;
+
+  std::cout << "\nInitialize time to Thursday, November 24, 2022 2:00:00 AM GMT+01:00..." << std::endl;
+  MyLocalTime novFirstTime(1669251600);
+  std::cout << "Calendar local time: " << novFirstTime.calendarTimeAsStr() << std::endl;
+  std::cout << "Epoch time: " << novFirstTime.epochTime() << std::endl;
   return 0;
 }
